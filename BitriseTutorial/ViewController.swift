4d5166b3460607e57eb8dc435fc58c5843dc69fd
icchii <i2s7.chacha@gmail.com>
2019-12-18 16:49:50 +0900

    Initial Commit

---
+++ BitriseTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitriseTutorial
+//
+//  Created by 市川星磨 on 2019/12/18.
+//  Copyright © 2019 市川 星磨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
