d8db19f16f9734c587cf833695d4b473d8b00e91
icchii <i2s7.chacha@gmail.com>
2019-12-18 17:05:51 +0900

    test commit

--- BitriseTutorial/ViewController.swift
+++ BitriseTutorial/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+func get(text: String) -> String {
+    return text
+}
--- BitriseTutorialTests/BitriseTutorialTests.swift
+++ BitriseTutorialTests/BitriseTutorialTests.swift
@@ -22,6 +22,8 @@ class BitriseTutorialTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCTAssertEqual("test", get(text: "test"))
     }
 
     func testPerformanceExample() {
@@ -30,5 +32,7 @@ class BitriseTutorialTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    
 
 }
